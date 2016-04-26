76beab9381c230f6299f68bf122c689d5b86ba99
Humpis <vmsailor@gmail.com>
2016-04-26 17:56:06 -0400

    initial commit

---
+++ hw4.asm
@@ -0,0 +1,43 @@
+# Homework #4
+# name: Vidar Minkovsky
+# sbuid: 109756598
+
+.text
+
+#Part 1
+
+#itof function
+itof:
+  jr $ra
+
+
+#Part 2
+
+#bears function
+bears:
+  jr $ra
+
+
+#Part 3
+
+#recursiveFindMajorityElement function
+recursiveFindMajorityElement:
+  jr $ra
+
+
+#iterateCandidates function
+iterateCandidates:
+  jr $ra
+
+
+#Part 4
+
+#recursiveFindLoneElement function
+recursiveFindLoneElement:
+  jr $ra
+
+
+
+
+### Data Section ###
+.data
